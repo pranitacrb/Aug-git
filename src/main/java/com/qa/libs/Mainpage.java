ca413c7a3812b4e27297a3e7220f2c94991c2d78
pranitacrb <pranita.mahajan@crbtech.in>
2020-02-13 12:36:58 +0530

    Code second try

---
+++ src/main/java/com/qa/libs/Mainpage.java
@@ -0,0 +1,13 @@
+package com.qa.libs;
+
+import org.openqa.selenium.WebDriver;
+
+public class Mainpage {
+
+	public static void main(String[] args) {
+		System.out.println("Mainpage");
+		WebDriver d;
+
+	}
+
+}
